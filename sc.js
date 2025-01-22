7af9c544f8e0863f17842297c93b196ae3f987ff
Jacob <jacobstudent29@gmail.com>
2025-01-22 20:23:22 +0100

    created shapeCalculator function

---
+++ sc.js
@@ -0,0 +1,8 @@
+var shapeCalculator = {
+    calculateArea(length, width) {
+        console.log("Current are is ", length * width, "m2")
+    },
+    calculatePerimeter(length, width) {
+        console.log("Current area is ", 2* length + 2 * width, "m")
+    }
+}
